38dd537c43e6ca98c1228256d89712626f75f005
Frank Leon Rose <frankleonrose@gmail.com>
2016-10-06 10:20:35 -0400

    Wrap Serial use in active-check macro

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,8 @@
 #include <hal/hal.h>
 #include <SPI.h>
 
+#define _SERIAL(op) if (Serial) { Serial.op; }
+
 // This EUI must be in little-endian format, so least-significant-byte
 // first. When copying an EUI from ttnctl output, this means to reverse
 // the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
@@ -67,90 +69,95 @@ const lmic_pinmap lmic_pins = {
 };
 
 void onEvent (ev_t ev) {
-    Serial.print(os_getTime());
-    Serial.print(": ");
+    _SERIAL(print(os_getTime()));
+    _SERIAL(print(": "));
     switch(ev) {
         case EV_SCAN_TIMEOUT:
-            Serial.println(F("EV_SCAN_TIMEOUT"));
+            _SERIAL(println(F("EV_SCAN_TIMEOUT")));
             break;
         case EV_BEACON_FOUND:
-            Serial.println(F("EV_BEACON_FOUND"));
+            _SERIAL(println(F("EV_BEACON_FOUND")));
             break;
         case EV_BEACON_MISSED:
-            Serial.println(F("EV_BEACON_MISSED"));
+            _SERIAL(println(F("EV_BEACON_MISSED")));
             break;
         case EV_BEACON_TRACKED:
-            Serial.println(F("EV_BEACON_TRACKED"));
+            _SERIAL(println(F("EV_BEACON_TRACKED")));
             break;
         case EV_JOINING:
-            Serial.println(F("EV_JOINING"));
+            _SERIAL(println(F("EV_JOINING")));
             break;
         case EV_JOINED:
-            Serial.println(F("EV_JOINED"));
+            _SERIAL(println(F("EV_JOINED")));
 
             // Disable link check validation (automatically enabled
             // during join, but not supported by TTN at this time).
             LMIC_setLinkCheckMode(0);
             break;
         case EV_RFU1:
-            Serial.println(F("EV_RFU1"));
+            _SERIAL(println(F("EV_RFU1")));
             break;
         case EV_JOIN_FAILED:
-            Serial.println(F("EV_JOIN_FAILED"));
+            _SERIAL(println(F("EV_JOIN_FAILED")));
             break;
         case EV_REJOIN_FAILED:
-            Serial.println(F("EV_REJOIN_FAILED"));
+            _SERIAL(println(F("EV_REJOIN_FAILED")));
             break;
             break;
         case EV_TXCOMPLETE:
-            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            _SERIAL(println(F("EV_TXCOMPLETE (includes waiting for RX windows)")));
             if (LMIC.txrxFlags & TXRX_ACK)
-              Serial.println(F("Received ack"));
+              _SERIAL(println(F("Received ack")));
             if (LMIC.dataLen) {
-              Serial.println(F("Received "));
-              Serial.println(LMIC.dataLen);
-              Serial.println(F(" bytes of payload"));
+              _SERIAL(println(F("Received ")));
+              _SERIAL(println(LMIC.dataLen));
+              _SERIAL(println(F(" bytes of payload")));
             }
             // Schedule next transmission
             os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
             break;
         case EV_LOST_TSYNC:
-            Serial.println(F("EV_LOST_TSYNC"));
+            _SERIAL(println(F("EV_LOST_TSYNC")));
             break;
         case EV_RESET:
-            Serial.println(F("EV_RESET"));
+            _SERIAL(println(F("EV_RESET")));
             break;
         case EV_RXCOMPLETE:
             // data received in ping slot
-            Serial.println(F("EV_RXCOMPLETE"));
+            _SERIAL(println(F("EV_RXCOMPLETE")));
             break;
         case EV_LINK_DEAD:
-            Serial.println(F("EV_LINK_DEAD"));
+            _SERIAL(println(F("EV_LINK_DEAD")));
             break;
         case EV_LINK_ALIVE:
-            Serial.println(F("EV_LINK_ALIVE"));
+            _SERIAL(println(F("EV_LINK_ALIVE")));
             break;
          default:
-            Serial.println(F("Unknown event"));
+            _SERIAL(println(F("Unknown event")));
             break;
     }
 }
 
 void do_send(osjob_t* j){
+  _SERIAL(print(convertSec(os_getTime())));
+  _SERIAL(print(": Do Send "));
+  _SERIAL(println(LMIC_getSeqnoUp()));
+
     // Check if there is not a current TX/RX job running
     if (LMIC.opmode & OP_TXRXPEND) {
-        Serial.println(F("OP_TXRXPEND, not sending"));
+        _SERIAL(println(F("OP_TXRXPEND, not sending")));
     } else {
         // Prepare upstream data transmission at the next possible time.
         LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
-        Serial.println(F("Packet queued"));
+        _SERIAL(println(F("Packet queued")));
     }
     // Next TX is scheduled after TX_COMPLETE event.
 }
 
 void setup() {
     Serial.begin(9600);
-    Serial.println(F("Starting"));
+    delay(100);     // per sample code on RF_95 test
+    _SERIAL(println(F("Starting")));
 
     #ifdef VCC_ENABLE
     // For Pinoccio Scout boards
